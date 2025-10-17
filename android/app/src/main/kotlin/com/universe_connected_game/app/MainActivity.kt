828a82b7a500640a7d82aca156acb81068664501
DamienLove <119716433+DamienLove@users.noreply.github.com>
2025-10-17 06:12:01 -0600

    Add MainActivity for Flutter app integration

---
+++ android/app/src/main/kotlin/com/universe_connected_game/app/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.universe_connected_game.app
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterFragmentActivity() {
+}
